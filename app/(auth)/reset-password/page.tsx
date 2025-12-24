df44f6cacce4630080dac1080df32469139b3f17
wametee <josephwamiti8711@gmail.com>
2025-12-24 23:15:35 +0300

    Updates

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -256,7 +256,7 @@ export default function ResetPassword() {
               {code.map((digit, index) => (
                 <input
                   key={index}
-                  ref={(el) => (inputRefs.current[index] = el)}
+                  ref={(el) => { inputRefs.current[index] = el; }}
                   type="text"
                   inputMode="numeric"
                   maxLength={1}
